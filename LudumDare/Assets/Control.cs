8668fb8036780cc6d77d85e41a4132957fcf0ddf
jim-works <jim4m80@gmail.com>
2023-09-29 16:18:59 -0400

    add movement

--- LudumDare/Assets/Control.cs
+++ LudumDare/Assets/Control.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class Control : MonoBehaviour
 {
-    public Vector2 JumpHeight;
+    public float JumpHeight = 5;
+    public float RunAcceleration = 1;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            GetComponent<Rigidbody2D>().velocity =JumpHeight;
+            rb.AddForce(new Vector2(0,JumpHeight), ForceMode2D.Impulse);
         }
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal")*RunAcceleration,0));
     }
 }
