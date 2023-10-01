b476c4e2cf699a144ff08b7c4c7d0d66fe0032af
jim-works <jim4m80@gmail.com>
2023-10-01 13:49:47 -0400

    integrate navmesh with wandering

--- LudumDare/Assets/Scripts/WanderMovementInput.cs
+++ LudumDare/Assets/Scripts/WanderMovementInput.cs
@@ -1,11 +1,13 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 [RequireComponent(typeof(Control))]
 public class WanderMovementInput : MonoBehaviour
 {
     public float WanderRadius = 2;
+    //smaller area to wander around if destination lands outside navmesh
+    public const float SAFE_WANDER_RADIUS = 0.5f;
     public float WanderPauseDuration = 2f;
     //set whenever component is enabled
     private Vector2 origin;
@@ -14,6 +16,7 @@ public class WanderMovementInput : MonoBehaviour
     private IEnumerator wanderingCoroutine;
     private Control control;
     private const float MOVEMENT_EPSILON = 0.25f;
+    private NavMeshPath path;
     // Start is called before the first frame update
     void Start()
     {
@@ -44,14 +47,41 @@ public class WanderMovementInput : MonoBehaviour
         if (delta.sqrMagnitude < MOVEMENT_EPSILON*MOVEMENT_EPSILON) {
             doneMoving = true;
         }
-        control.MoveDirection =  delta;
+        if (path == null)
+        {
+            //walk in safe area if no path
+            control.MoveDirection = delta;
+        }
+        else
+        {
+            Vector3 d = path.corners[1] - transform.position;
+            if (d.sqrMagnitude < 0.001f && path.corners.Length > 2)
+            {
+                d = path.corners[2] - transform.position;
+            }
+            control.MoveDirection = d.normalized;
+        }
     }
 
     private IEnumerator SetDestination() {
         while(true) {
             yield return new WaitForSeconds(WanderPauseDuration);
             wanderDestination = origin + Random.insideUnitCircle*WanderRadius;
+            UpdatePath(wanderDestination);
             doneMoving = false;
+        }
+    }
+
+    void UpdatePath(Vector2 destination)
+    {
+        path = new();
+        NavMesh.SamplePosition(transform.position, out NavMeshHit hitA, 10f, NavMesh.AllAreas);
+        NavMesh.SamplePosition(destination, out NavMeshHit hitB, 10f, NavMesh.AllAreas);
+        if (!hitA.hit || !hitB.hit || !NavMesh.CalculatePath(hitA.position, hitB.position, NavMesh.AllAreas, path))
+        {
+            //do safe small radius wandering if no path
+            path = null;
+            wanderDestination = origin + Random.insideUnitCircle*SAFE_WANDER_RADIUS;
         }
     }
 }
